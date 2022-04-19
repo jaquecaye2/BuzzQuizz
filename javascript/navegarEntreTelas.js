7fb40e39f55cf267147dc7822e93fd60be0a227a
jaquecaye <jaquelinecaye@gmail.com>
2022-04-19 14:26:47 -0300

    Layout: Tela 1 e 2

---
+++ javascript/navegarEntreTelas.js
@@ -0,0 +1,7 @@
+function abrirQuiz(elemento){
+    document.querySelector("section.tela2").classList.remove("escondido")
+}
+
+function voltarHome(){
+    document.querySelector("section.tela2").classList.add("escondido")
+}
