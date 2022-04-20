fe116330bb49caf30f612034646610f061fffbff
jaquecaye <jaquelinecaye@gmail.com>
2022-04-20 17:48:26 -0300

    Criar Quiz: Validação tela 3.1

--- javascript/criarQuiz.js
+++ javascript/criarQuiz.js
@@ -1,15 +1,27 @@
+let tituloQuiz
+let URLimagemQuiz
+let quantPerguntas
+let quantNiveis
+
+function isValidURL(string) {
+    let res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+}
+
 function validacaoTelaInfoBasicas(){
-    let tituloQuiz = document.querySelector("#tituloQuiz").value
+    tituloQuiz = document.querySelector("#tituloQuiz").value
     console.log(tituloQuiz.length)
-    let URLimagemQuiz = document.querySelector("#URLimagemQuiz").value
-    let quantPerguntas = document.querySelector("#quantPerguntas").value
-    let quantNiveis = document.querySelector("#quantNiveis").value
+    URLimagemQuiz = document.querySelector("#URLimagemQuiz").value
+    quantPerguntas = document.querySelector("#quantPerguntas").value
+    quantNiveis = document.querySelector("#quantNiveis").value
 
     if (tituloQuiz.length < 20 || tituloQuiz.length > 65) {
         alert("Informação inserida incorretamente, tente novamente")
     } else if (quantPerguntas < 3) {
         alert("Informação inserida incorretamente, tente novamente")
     } else if (quantNiveis < 2) {
+        alert("Informação inserida incorretamente, tente novamente")
+    } else if (!isValidURL(URLimagemQuiz)) {
         alert("Informação inserida incorretamente, tente novamente")
     } else {
         scrolarTopPaginaForm()
