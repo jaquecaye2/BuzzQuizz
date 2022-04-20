79a18d8a8665d4a6700846734b5302b8ef3edc0d
jaquecaye <jaquelinecaye@gmail.com>
2022-04-19 23:16:43 -0300

    Layout: Tela 3 estática

--- javascript/navegarEntreTelas.js
+++ javascript/navegarEntreTelas.js
@@ -1,11 +1,28 @@
-function abrirQuiz(elemento){
+function abrirQuiz(){
     document.querySelector("section.tela2").classList.remove("escondido")
+    document.querySelector("section.tela3").classList.add("escondido")
 }
 
 function voltarHome(){
     document.querySelector("section.tela2").classList.add("escondido")
+    document.querySelector("section.tela3").classList.add("escondido")
 }
 
 function criarQuiz(){
     document.querySelector("section.tela3").classList.remove("escondido")
+}
+
+function passarParaTela2Form(){
+    document.querySelector(".containerTela3-1").classList.add("escondido")
+    document.querySelector(".containerTela3-2").classList.remove("escondido")
+}
+
+function passarParaTela3Form(){
+    document.querySelector(".containerTela3-2").classList.add("escondido")
+    document.querySelector(".containerTela3-3").classList.remove("escondido")
+}
+
+function passarParaTela4Form(){
+    document.querySelector(".containerTela3-3").classList.add("escondido")
+    document.querySelector(".containerTela3-4").classList.remove("escondido")
 }
