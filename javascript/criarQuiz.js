dbf0d0c6c1e2e6ea058b1aeb228fecd81a5371bb
jaquecaye <jaquelinecaye@gmail.com>
2022-04-20 16:05:50 -0300

    Teste

--- javascript/criarQuiz.js
+++ javascript/criarQuiz.js
@@ -3,4 +3,5 @@ function validacaoTelaInfoBasicas(){
     console.log(document.querySelector("#URLimagemQuiz").value)
     console.log(document.querySelector("#quantPerguntas").value)
     console.log(document.querySelector("#quantNiveis").value)
+    
 }
