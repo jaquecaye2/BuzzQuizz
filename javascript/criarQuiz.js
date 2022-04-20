0d16ea65b92b79a1734a81770ca59c3b679fdb2a
jaquecaye <jaquelinecaye@gmail.com>
2022-04-20 17:37:56 -0300

    Criar Quiz: inicio de validação tela 3.1

--- javascript/criarQuiz.js
+++ javascript/criarQuiz.js
@@ -1,7 +1,19 @@
 function validacaoTelaInfoBasicas(){
     let tituloQuiz = document.querySelector("#tituloQuiz").value
-    console.log(document.querySelector("#URLimagemQuiz").value)
-    console.log(document.querySelector("#quantPerguntas").value)
-    console.log(document.querySelector("#quantNiveis").value)
-    
+    console.log(tituloQuiz.length)
+    let URLimagemQuiz = document.querySelector("#URLimagemQuiz").value
+    let quantPerguntas = document.querySelector("#quantPerguntas").value
+    let quantNiveis = document.querySelector("#quantNiveis").value
+
+    if (tituloQuiz.length < 20 || tituloQuiz.length > 65) {
+        alert("Informação inserida incorretamente, tente novamente")
+    } else if (quantPerguntas < 3) {
+        alert("Informação inserida incorretamente, tente novamente")
+    } else if (quantNiveis < 2) {
+        alert("Informação inserida incorretamente, tente novamente")
+    } else {
+        scrolarTopPaginaForm()
+        document.querySelector(".containerTela3-1").classList.add("escondido")
+        document.querySelector(".containerTela3-2").classList.remove("escondido")
+    }
 }
